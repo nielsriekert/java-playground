b92773aa0c7d1e6468907ba5d6dcc9eced2c5ee4
nielsriekert <niels.riekert@gmail.com>
2016-11-28 21:48:30 +0100

    DotComGame inc ArrayList

---
+++ DotComGame.java
@@ -0,0 +1,23 @@
+public class DotComGame {
+	public static void main(String[] args) {
+		int numberOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+
+		DotCom theDotCom = new DotCom();
+		int randomNum = (int) (Math.random() * 5);
+
+		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		theDotCom.setLocationCells(locations);
+		boolean isAlive = true;
+
+		while(isAlive == true){
+			String guess = helper.getUserInput("enter a number");
+			String result = theDotCom.checkYourself(guess);
+			numberOfGuesses++;
+			if(result.equals("kill")) {
+				isAlive = false;
+				System.out.println("You took " + numberOfGuesses + " guesses");
+			}
+		}
+	}
+}
